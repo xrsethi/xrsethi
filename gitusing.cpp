3b626e7d48e0b238eb20777cced996930d690323
xrsethi <xr.sethi@gmail.com>
2022-02-09 16:45:29 +0800

    git use

---
+++ gitusing.cpp
@@ -0,0 +1,12 @@
+/*
+*git 使用技巧
+* github trend https://github.com/trending 总结最近热门存储和开发者
+* github Tipic https://github.com/topics   展示最新和主流的讨论主题
+* 
+* 搜索项目 关键字
+* Awesome + 关键字  帮助找到最优秀的工具列表
+* stars:>=500 匹配收藏数量超过 500 的项目
+* language:javascript 匹配以javascript作为开发语言的项目
+* forks:>=500 匹配分支数量唱过500的项目  forks:100..500
+* location:china  匹配用户填写的地址在 china
+*/
