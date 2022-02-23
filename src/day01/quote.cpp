e9ca2b4fd7ea64640fc159eaff32fc9ed67afdd9
xrsethi <xr.sethi@gmail.com>
2022-02-23 08:43:22 +0800

    quote fun

--- src/day01/quote.cpp
+++ src/day01/quote.cpp
@@ -37,6 +37,7 @@ int main992()
 	int y = 11;
 	mycppswap(x, y);
 	cout << "x = " << x << " y = " << y << endl;
+	return 0;
 }
 //并不能实现，因为作用域不同，栈不同
 void myswap(int a, int b)
@@ -55,4 +56,41 @@ void mycppswap(int& a, int& b)
 	b = c;
 }
 
-//continue 19 引用 复杂数据类型引用做函数参数 
+//continue 19 引用 复杂数据类型的引用（函数参数 ）
+
+/*
+*	printfT()、 printfT1()、 printfT2()、
+*   三个函数有天然区别，见下述main()
+*/
+struct Teacher
+{
+	char name[64];
+	int  age;
+};
+void printfT(Teacher* pT)
+{
+	cout << pT->age << endl;
+}
+void printfT1(Teacher& pT)
+{
+	cout << pT.age << endl;
+	//pT.age = 45;
+}
+void printfT2(Teacher pT)
+{
+	cout << pT.age << endl;
+	pT.age = 55;
+}
+int main()
+{
+	Teacher t1;
+	t1.age = 35;
+
+	printfT(&t1); 
+	printfT1(t1);	//pT是t1的别名,修改pT相当于修改t1
+	printfT2(t1);	//pT是形参，t1 copy 一份数据给pT 相当于 pT = t1;(这里有点意思，后续可见)
+	printf("t1.age:%d \n",t1.age);
+	return 0;
+}
+
+//continue 20 引用的意思和引用的本质
